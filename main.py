b8aa7c09cb9e334084afd02ea76fb62bb218c0ed
jamesapp <emindujacques1311e@gmail.com>
2026-03-24 19:11:07 +0000

    mise a jour 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 import time
+import plotly.graph_objects as go  # 🟢 AJOUTÉ : Pour les graphiques avec échelle fixe
 from fpdf import FPDF
 import datetime
 
